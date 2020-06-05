303eb155904694193ece1eeb0bb0abed5804279a
tamilsng <48792190+tamilsng@users.noreply.github.com>
2020-06-05 16:13:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,11 +28,5 @@ def main():
 def new_color(new_color):
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
 
-@app.route('/read_file')
-def read_file():
-    f = open("/data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
